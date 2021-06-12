219779f2a3ba38cc20a09bdf13e4b67f6d61fe9e
Ana0609 <85016977+Ana0609@users.noreply.github.com>
2021-06-12 13:07:47 +0300

    added code for list

--- BirthdayProject/src/app/app.component.spec.ts
+++ BirthdayProject/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'BirthdayProject'`, () => {
+  it(`should have as title 'empty'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('BirthdayProject');
+    expect(app.title).toEqual('empty');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('BirthdayProject app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('Welcome to empty!');
   });
 });
